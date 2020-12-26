07a68fe49204e7f50489657398877d62f23945d4
pruss <31600998+zxc30314@users.noreply.github.com>
2020-12-26 21:30:01 +0800

    Update

---
+++ Assets/Tutorial/PanelDemo/Manager.cs
@@ -0,0 +1,51 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Panel
+{
+    public class Manager : MonoBehaviour
+    {
+        [SerializeField] private Panel panelPrefab;
+        // Start is called before the first frame update
+        void Start()
+        {
+            string[] data = new string[] { "0", "1", "2" };
+            OpenPanels(data);
+        }
+
+        private void OpenPanels(string[] data)
+        {
+            List<Action> actions = new List<Action>();
+            var count = 0;
+            foreach (var item in data)
+            {
+                Action action = () => OpenPanels(item, OnNext);
+                actions.Add(action);
+            }
+            OnNext();
+            void OnNext()
+            {
+                if (count >= data.Length)
+                {
+                    return;
+                }
+                actions[count++]?.Invoke();
+            }
+        }
+
+        private void OpenPanels(string item, TweenCallback onNext)
+        {
+            var go = Instantiate(panelPrefab, transform);
+            go.Open(item, onNext);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Tutorial/PanelDemo/Panel.cs
@@ -0,0 +1,49 @@
+﻿using DG.Tweening;
+using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Panel
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class Panel : MonoBehaviour
+    {
+        private RectTransform rectTransform;
+        public RectTransform RectTransform => rectTransform ? rectTransform : rectTransform = GetComponent<RectTransform>();
+        [SerializeField] private Button button;
+        [SerializeField] private Text text;
+        [SerializeField] private Text timer;
+        TweenCallback closeEvent;
+        private long closeDelay = 5;
+        public void Open(string item, TweenCallback onClosed)
+        {
+            text.text = item;
+            closeEvent = onClosed;
+            RectTransform.localScale = Vector3.zero;
+            RectTransform.DOScale(1, 0.5f);
+            button.onClick.AddListener(Close);
+            Observable.Interval(TimeSpan.FromSeconds(1)).Where(value => value <= closeDelay).Subscribe(OnNext).AddTo(this);
+        }
+
+        private void OnNext(long index)
+        {
+            var temp = closeDelay - index;
+            timer.text = temp.ToString();
+            if (temp == 0)
+            {
+                Close();
+            }
+        }
+
+        public void Close()
+        {
+            RectTransform.DOScale(0, 0.5f).OnComplete(() =>
+            {
+                closeEvent?.Invoke();
+                Destroy(gameObject);
+            });
+
+        }
+    }
+}
